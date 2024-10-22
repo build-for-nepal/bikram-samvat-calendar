8153d815ccb7032ecd38c50d4ed32f65b44e947e
Kapil Karki <lotuskapil31@gmail.com>
2024-10-22 11:24:22 +0545

    build cache clear

--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-import Calendar from "./CalendarComponent/Calendar";
-export { Calendar };
--- build/util/calendar.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const generateDate: (month?: number, year?: number) => any[];
-export declare const months: string[];
