043447978b4e04c0fc4e8cacd248a1f69bb29f1d
kapil-31 <60149165+kapil-31@users.noreply.github.com>
2024-10-28 09:59:02 +0545

    Feat/datepicker (#6)
    * adding prettier
    
    * customization
    
    * husky precommit hooks
    
    * husky precommit hooks
    
    * cleaning code
    
    * calender theme props for changes theming
    
    * formatting
    
    * add converterjs for nepali dates
    
    * fix: showin nepali dates
    
    * fix: showin nepali dates
    
    * nepali calender with dates
    
    * nepali calender with dates
    
    * type correction
    
    * feat: using nepali-date-converter package
    
    * feat: using nepali-date-converter package  for dates
    
    * fix; removing fouccs
    
    * date picker
    
    * date picker
    
    * date picker with nepali dates
    
    * date picker with nepali dates
    
    * removed unused component
    
    * removed unused component

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,44 +1,35 @@
-import dayjs from 'dayjs';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { enToNpNum, generateDate, months, nepaliDaysName } from '../util/calendar';
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
 import cn from '../util/cn';
 import { CalendarProps } from '../types/Calender';
 import NepaliDate from 'nepali-date-converter';
-var adbs = require('ad-bs-converter');
 const MAX_NP_YEAR = 2089;
 const MIN_NP_YEAR = 2000;
 export default function Calendar({
   wrapperClass = '',
-  theme = {
-    today: 'text-white',
-    currentMonth: 'text-gray-600',
-    selected: 'bg-primary text-white',
-    hover: 'hover:bg-muted hover:text-white',
-    header: 'font-semibold',
-    dayHeader: 'text-sm text-center text-gray-500',
-  },
+  theme,
   onChange,
+  value = new Date(),
 }: CalendarProps) {
-  const currentDate = new NepaliDate();
-  const [today, setToday] = useState<NepaliDate | null>(currentDate);
+  const currentDate = new NepaliDate(value);
   const [selectedMonth, setSelectedMonth] = useState(currentDate.getMonth());
   const [selectedYear, setSelectedYear] = useState(currentDate.getYear());
-
+  const [selectedDate, setSelectedDate] = useState<NepaliDate | null>(currentDate);
   const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const month = Number(event.target.value);
-    setToday(null);
+    setSelectedDate(null);
     setSelectedMonth(month);
   };
 
   const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const year = parseInt(event.target.value);
-    setToday(null);
+    setSelectedDate(null);
     setSelectedYear(year);
   };
 
   const handleNextYear = () => {
-    setToday(null);
+    setSelectedDate(null);
     setSelectedYear((prev) => {
       const isMaxYearGreater = prev + 1 <= MAX_NP_YEAR;
       return isMaxYearGreater ? prev + 1 : prev;
@@ -46,7 +37,7 @@ export default function Calendar({
   };
 
   const handlePrevYear = () => {
-    setToday(null);
+    setSelectedDate(null);
     setSelectedYear((prev) => {
       const isMinYearGreater = prev - 1 >= MIN_NP_YEAR;
       return isMinYearGreater ? prev - 1 : prev;
@@ -54,26 +45,22 @@ export default function Calendar({
   };
   const handleSelecteDate = (date: NepaliDate) => {
     onChange(date);
-    setToday(date);
+    setSelectedDate(date);
   };
   const resetDateToToday = () => {
     setSelectedYear(currentDate.getYear());
     setSelectedMonth(currentDate.getMonth());
-    setToday(currentDate);
   };
-
-  useEffect(() => {
-    onChange(currentDate);
-  }, []);
-
   return (
-    <div className={cn('w-[550px] border border-collapse shadow-md', wrapperClass)}>
-      <div className="flex justify-between p-2 items-center mb-4">
+    <div
+      className={cn('min-w-[400px] w-fit border border-collapse shadow-md bg-white', wrapperClass)}
+    >
+      <div className={cn('flex justify-between p-2 items-center mb-4', theme.header)}>
         <div className="flex items-center">
           <select
             value={selectedYear}
             onChange={handleYearChange}
-            className="p-2 border rounded focus-visible::outline-none"
+            className="p-2 border rounded focus:outline-none focus-visible::outline-none "
           >
             {Array.from({ length: 90 }, (_, i) => {
               return {
@@ -88,7 +75,7 @@ export default function Calendar({
           <select
             value={selectedMonth}
             onChange={handleMonthChange}
-            className="ml-2 p-2 border rounded focus-visible::outline-none"
+            className="ml-2 p-2 border rounded focus:outline-none focus-visible::outline-none"
           >
             {months.monthsName.map((month, index) => (
               <option key={index} value={index}>
@@ -115,40 +102,43 @@ export default function Calendar({
           />
         </div>
       </div>
-      <div className="grid  h-14 grid-cols-7 bg-[#ffffff] shadow-sm select-none">
+      <div className={cn('grid grid-cols-7  bg-[#ffffff] shadow-sm select-none')}>
         {nepaliDaysName.map((day, index) => (
           <h1
             key={index}
-            className={cn('h-15 w-15 border-t grid place-content-center bg-', theme.dayHeader)}
+            className={cn(
+              'h-15 w-15 text-sm  p-2 border-t grid place-content-center text-center ',
+              theme.dayHeader
+            )}
           >
             {day}
           </h1>
         ))}
       </div>
 
-      <div className="grid grid-cols-7 border-collapse">
-        {generateDate(selectedMonth, selectedYear).map(({ date, currentMonth }, index) => {
+      <div className="grid grid-cols-7  border-collapse">
+        {generateDate(selectedMonth, selectedYear).map(({ date }, index) => {
+          const today = currentDate.toJsDate().toDateString() == date.toJsDate().toDateString();
+          const selected =
+            selectedDate?.toJsDate().toDateString() == date.toJsDate().toDateString();
           return (
             <div
               key={index}
               onClick={() => handleSelecteDate(date)}
               className={cn(
-                'p-2 cursor-pointer  transition-all text-center h-14 border-b border-r   grid place-content-center text-sm ',
+                'cursor-pointer   h-14 w-14 transition-all text-center  border-b border-r   grid place-content-center text-sm ',
                 {
-                  [theme.selected]:
-                    today?.toJsDate().toDateString() == date.toJsDate().toDateString(),
-                  // ' cursor-not-allowed ': !currentMonth,
-                  [theme.hover]: currentMonth && !today,
-                }
+                  [`text-primary ${theme.today}`]: today,
+                  [`bg-primary text-black ${theme.selected}`]: selected,
+                  [`hover:bg-muted  ${theme.hover}`]: !selected,
+                },
+                theme.dateGrid
               )}
             >
               <h1
                 className={cn(
-                  'h-15 w-15 rounded-full text-md font-semibold grid place-content-center transition-all  select-none',
+                  'h-full w-full rounded-full text-md font-semibold grid place-content-center transition-all  select-none',
                   {
-                    [theme.today]: today,
-                    'opacity-50 text-grey-300': !currentMonth,
-                    [theme.currentMonth]: currentMonth,
                     'text-red-500': (index + 1) % 7 == 0,
                   }
                 )}
